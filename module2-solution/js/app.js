f2f6fc780536d266591d2b76f94d9bc86f3fca79
Bottani Simone Consulente <cons.BottaniSimone@sia.eu>
2017-01-12 20:51:26 +0100

    Module 2 solution

---
+++ module2-solution/js/app.js
@@ -0,0 +1,61 @@
+angular.module('ShoppingListCheckOff', []).controller('ToBuyController',
+		ToBuyController).controller('AlreadyBoughtController',
+		AlreadyBoughtController).service('ShoppingListCheckOffService',
+		ShoppingListCheckOffService);
+
+ToBuyController.$inject = [ 'ShoppingListCheckOffService' ];
+AlreadyBoughtController.$inject = [ 'ShoppingListCheckOffService' ];
+ShoppingListCheckOffService.$inject = [];
+
+function ShoppingListCheckOffService() {
+
+	var service = this;
+	var toBuyList = [ {
+		name : "cookie",
+		quantity : 10
+	}, {
+		name : "apple",
+		quantity : 2
+	}, {
+		name : "orange",
+		quantity : 5
+	}, {
+		name : "salad",
+		quantity : 7
+	}, {
+		name : "cake",
+		quantity : 8
+	} ];
+
+	var boughtList = [];
+	
+	service.getToBuyList = function() {
+		return toBuyList;
+	}
+	
+	service.getBoughtList = function() {
+		return boughtList;
+	}
+
+	service.buy = function(index) {
+		boughtList.push(toBuyList[index]);
+		toBuyList.splice(index, 1);
+	}
+
+}
+
+function ToBuyController(ShoppingListCheckOffService) {
+
+	//$scope.toBuyList = ShoppingListCheckOffService.toBuyList;
+	var toBuy = this;
+	toBuy.list = ShoppingListCheckOffService.getToBuyList();
+	this.buy = function(item){
+		// alert(item);
+		ShoppingListCheckOffService.buy(item);
+	}
+}
+
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+	var bought = this;
+	bought.list = ShoppingListCheckOffService.getBoughtList();
+}
